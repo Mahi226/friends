004c817f2997ab11ac6e1effaa648ab9b2d4db53
MAHESH DUGGILLA <136039433+Mahi226@users.noreply.github.com>
2024-03-31 22:44:02 +0530

    Add files via upload

---
+++ mahi1.js
@@ -0,0 +1,37 @@
+let name = document.getElementById('name')
+let img  = document.getElementById('image')
+
+
+
+
+
+function sai()  {
+	name.textContent = "Sai Kumar"
+	img.setAttribute("src","https://res.cloudinary.com/djuqhfvux/image/upload/v1711903730/AREmoji_20230906_033421_umwhc1.jpg")
+
+
+}
+
+function manasa()  {
+	name.textContent = "Manasa"
+	img.setAttribute("src","https://res.cloudinary.com/djuqhfvux/image/upload/v1711903710/AREmoji_20230906_031806_rc40my.jpg")
+
+}
+
+function vishwa()  {
+	name.textContent = "Vishwa"
+	img.setAttribute("src","https://res.cloudinary.com/djuqhfvux/image/upload/v1711903737/AREmoji_20230906_033506_zabhyq.jpg")
+
+}
+
+function mahi(){
+	name.textContent = "Mahi"
+	img.setAttribute("src","https://res.cloudinary.com/djuqhfvux/image/upload/v1711903718/AREmoji_20230906_032219_n9osoc.jpg")
+
+}	
+
+function group(){
+	name.textContent = "Group Pic"
+	img.setAttribute("src","https://res.cloudinary.com/djuqhfvux/image/upload/v1711903688/IMG_20230906_041909067_hgbmcq.jpg")
+
+}
